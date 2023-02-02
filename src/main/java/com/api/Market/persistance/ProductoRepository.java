59ec4ec1e5058a129262d8d3f265d4c9109996e0
Cesar <cesar_a_mar_@hotmail.com>
2023-02-02 17:45:24 -0500

    Implementar la anotacion Service

--- src/main/java/com/api/Market/persistance/ProductoRepository.java
+++ src/main/java/com/api/Market/persistance/ProductoRepository.java
@@ -5,13 +5,16 @@ import com.api.Market.domain.repository.ProductRepository;
 import com.api.Market.persistance.crud.ProductoCrudRepository;
 import com.api.Market.persistance.entity.Producto;
 import com.api.Market.persistance.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired //sirve para inicializar el objecto
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
     @Override
     public List<Product> getAll() {
